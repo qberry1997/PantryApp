040115e6345896dbda86df39076cb7f09673bc30
Quinn Berry <qberry@asu.edu>
2020-11-24 15:39:01 -0700

    Changed "ViewController" to "ItemViewController"

--- FInalApp/FInalApp/ItemViewController.swift
+++ FInalApp/FInalApp/ItemViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ItemViewController.swift
 //  FInalApp
 //
 //  Created by Quinn Berry on 11/24/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ItemViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
