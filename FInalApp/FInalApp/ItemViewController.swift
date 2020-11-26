0e3cd54d45ea5d69986b202f040e2779ceac0f3f
Quinn Berry <qberry@asu.edu>
2020-11-25 18:40:54 -0700

    Added functionality for ItemViewController. The view now displays relevant information about the item and allows you to update current/threshold values.

--- FInalApp/FInalApp/ItemViewController.swift
+++ FInalApp/FInalApp/ItemViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class ItemViewController: UIViewController {
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var photoSegControl: UISegmentedControl!
+    @IBOutlet weak var itemNameLbl: UILabel!
+    @IBOutlet weak var currentTxt: UITextField!
+    @IBOutlet weak var thresholdTxt: UITextField!
+    @IBOutlet weak var priceLbl: UILabel!
+    
     var selectedItem:PantryItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        loadLabels()
         // Do any additional setup after loading the view.
+    }
+    
+    func loadLabels() {
+        itemNameLbl.text = selectedItem?.getName()
+        let current = selectedItem?.getCurrent()
+        let threshold = selectedItem?.getThreshold()
+        currentTxt.text = String(current!)
+        thresholdTxt.text = String(threshold!)
+        
+        if let image = selectedItem?.getImage() {
+            itemImage.image = UIImage(data: image)
+        }
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func saveChanges(_ sender: Any) {
+        let current = Double(currentTxt.text!)
+        let threshold = Double(thresholdTxt.text!)
+        if let image = itemImage.image {
+            selectedItem?.setImage(image: image.pngData()!)
+        }
+        selectedItem?.setCurrent(current: current!)
+        selectedItem?.setThreshold(threshold: threshold!)
     }
 }
